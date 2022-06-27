3501de3a97c490889147564be9059082b92e02a1
Yonatan Desta <ydesta@fb.com>
2022-06-27 14:05:27 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Yonatan Desta on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
